de3f69f0a7f8c30d7276a64118983cbab5438002
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-06-14 08:23:27 +0100

    Add new action to SA_Orders controller to create a new SA_LineItems

--- modules/SA_Orders/controller.php
+++ modules/SA_Orders/controller.php
@@ -18,4 +18,42 @@ class SA_OrdersController extends SugarController
 
         $this->bean->overdue = $delivery_date < $now;
     }
+
+    public function action_add_line_items()
+    {
+        // Is this the proper way of not returning any html?
+        $this->view = '';
+        $bean = $this->get_line_item_bean_from_post();
+        if ($bean === null) {
+            die('error');
+        }
+        $bean->save();
+    }
+
+    /**
+     * @return SA_LineItems|null
+     */
+    private function get_line_item_bean_from_post()
+    {
+        /** @var SA_LineItems $bean */
+        $bean = BeanFactory::newBean('SA_LineItems');
+
+        // TODO is there a built-in validation I could use?
+
+        $bean->name = filter_input(INPUT_POST, 'line-item-name', FILTER_SANITIZE_STRING);
+        $bean->quantity = filter_input(INPUT_POST, 'line-item-quantity', FILTER_VALIDATE_INT);
+        $bean->price = filter_input(INPUT_POST, 'line-item-price', FILTER_VALIDATE_FLOAT);
+        $bean->order_id = filter_input(INPUT_POST, 'line-item-order-id', FILTER_SANITIZE_STRING);
+
+        if (
+            empty($bean->name) ||
+            empty($bean->quantity) ||
+            empty($bean->price) ||
+            empty($bean->order_id)
+        ) {
+            return null;
+        }
+
+        return $bean;
+    }
 }
